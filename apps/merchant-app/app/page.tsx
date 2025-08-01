1ca2757cb08af61859607ec8e7307ff7fd0a28f4
KheteshS <kheteshs@gmail.com>
2025-08-01 15:14:30 +0530

    initial setup of project

---
+++ apps/merchant-app/app/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <div className="text-2xl">Merchant app's Page.tsx</div>;
+}
---
+++ apps/user-app/app/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <div className="text-2xl">User's app Page.tsx</div>;
+}
